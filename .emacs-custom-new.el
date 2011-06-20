a00af284e57cac9f539ae9682ea96a2c575ceae7
Zane Ashby <zane.a@demonastery.org>
2011-06-21 09:03:34 +1200

    Updated .emacs stuff

---
+++ .emacs-custom-new.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(display-time-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(transient-mark-mode nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(eval-sexp-fu-flash ((((class color)) (:background "#ddccff"))))
+ '(fringe ((((class color) (background light)) (:background "white"))))
+ '(org-meta-line ((t (:inherit font-lock-comment-face :foreground "#cccccc" :slant italic))))
+ '(variable-pitch ((t (:height 78 :weight normal :family "drift")))))
+ ;'(variable-pitch ((t (:height 98 :weight bold :family "Terminal")))))
+; '(variable-pitch ((t (:height 98 :weight bold :family "smoothansi")))))
+; '(variable-pitch ((t (:height 98 :weight bold :family "glisp")))))
+ ;'(variable-pitch ((t (:height 98 :family "Helvetica Neue")))))
+ ;'(variable-pitch ((t (:height 98 :weight light :family "Helvetica Neue")))))
