556dc2f4f584d527b49d0aa64227c2be614e0bbd
Zane Ashby <zane.a@demonastery.org>
2013-04-30 00:17:19 +1200

    Remove everything. Files are stale as hell.

--- chicken-wmii/themes/live-coding.scm
+++
@@ -1,9 +0,0 @@
-;;
-;; Purple live-coding theme
-;;
-
-(define wmii-normcolors  '(#xcb8cff #x242424 #x303030)) 
-(define wmii-focuscolors '(#xcb8cff #x332340 #x303030))
-(define wmii-evalcolors  '(#xaa8888 #x242424 #x242424))
-
-(define wmii-font "xft:Geneva-8")
--- chicken-wmii/theming.scm
+++
@@ -1,9 +0,0 @@
-;;
-;; Theming
-;;
-
-(define (load-theme path)
-  (load (conc "~/.wmii/chicken-wmii/themes/" path ".scm"))
-  (add-setting font wmii-font)
-  (add-setting focuscolors wmii-focuscolors)
-  (add-setting normcolors wmii-normcolors))
